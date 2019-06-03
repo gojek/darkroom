23e3c8aea038404a89a5168c6e8b930d4e3e9303
Ajat Prabha <ajat.p.aux@go-jek.com>
2019-06-03 16:16:03 +0530

    improve logger coverage

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -4,12 +4,13 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
+	"net/http"
 	"testing"
 )
 
 func TestLogger(t *testing.T) {
 	hook := test.NewLocal(getLogger())
-	getLogger().AddHook(hook)
+	AddHook(hook)
 
 	cases := []struct {
 		message  string
@@ -68,34 +69,34 @@ func TestLogger(t *testing.T) {
 
 func TestLoggerFormatter(t *testing.T) {
 	hook := test.NewLocal(getLogger())
-	getLogger().AddHook(hook)
+	AddHook(hook)
 
 	cases := []struct {
-		format  string
+		format   string
 		level    logrus.Level
 		args     []string
 		callFunc func(format string, args ...interface{})
 	}{
 		{
-			format:  "warning message: %s",
+			format:   "warning message: %s",
 			level:    logrus.WarnLevel,
 			args:     []string{"WARNING"},
 			callFunc: Warnf,
 		},
 		{
-			format:  "debug message: %s",
+			format:   "debug message: %s",
 			level:    logrus.DebugLevel,
 			args:     []string{"DEBUG"},
 			callFunc: Debugf,
 		},
 		{
-			format:  "error message: %s",
+			format:   "error message: %s",
 			level:    logrus.ErrorLevel,
 			args:     []string{"ERROR"},
 			callFunc: Errorf,
 		},
 		{
-			format:  "info message: %s",
+			format:   "info message: %s",
 			level:    logrus.InfoLevel,
 			args:     []string{"INFO"},
 			callFunc: Infof,
@@ -107,4 +108,45 @@ func TestLoggerFormatter(t *testing.T) {
 		assert.Equal(t, (i+1)*2, len(hook.Entries))
 		assert.Equal(t, c.level, hook.LastEntry().Level)
 	}
+}
+
+func TestLoggerWithFields(t *testing.T) {
+	hook := test.NewLocal(getLogger())
+	AddHook(hook)
+
+	msg := "WithFields"
+
+	fields := make(map[string]interface{})
+	fields["key"] = "value"
+	WithFields(fields).Warn(msg)
+
+	assert.Equal(t, 2, len(hook.Entries))
+	assert.Equal(t, logrus.WarnLevel, hook.LastEntry().Level)
+	assert.Equal(t, msg, hook.LastEntry().Message)
+}
+
+func TestLoggerWithField(t *testing.T) {
+	hook := test.NewLocal(getLogger())
+	AddHook(hook)
+
+	msg := "WithField"
+
+	WithField("key", "value").Warn(msg)
+
+	assert.Equal(t, 2, len(hook.Entries))
+	assert.Equal(t, logrus.WarnLevel, hook.LastEntry().Level)
+	assert.Equal(t, msg, hook.LastEntry().Message)
+}
+
+func TestLoggerWithRequest(t *testing.T) {
+	hook := test.NewLocal(getLogger())
+	AddHook(hook)
+	msg := "success"
+	req, _ := http.NewRequest(http.MethodGet, "https://example.com/path/to/resource", nil)
+
+	WithRequest(req).Info(msg)
+
+	assert.Equal(t, 2, len(hook.Entries))
+	assert.Equal(t, logrus.InfoLevel, hook.LastEntry().Level)
+	assert.Equal(t, msg, hook.LastEntry().Message)
 }
