9bd3380331b67d558c793b33ade7d10e8ccb387e
Ajat Prabha <ajat.p.aux@go-jek.com>
2019-06-05 16:46:16 +0530

    add Manipulator to handler

--- handler/image.go
+++ handler/image.go
@@ -17,8 +17,27 @@ func ImageHandler(deps *service.Dependencies) http.HandlerFunc {
 			w.WriteHeader(res.Status())
 			return
 		}
+		var data []byte
+		var err error
+		data = res.Data()
 
-		cl, _ := w.Write([]byte(res.Data()))
+		params := make(map[string]string)
+		values := r.URL.Query()
+		if len(values) > 0 {
+			for v := range values {
+				if len(values.Get(v)) != 0 {
+					params[v] = values.Get(v)
+				}
+			}
+			data, err = deps.Manipulator.Process(r.Context(), data, params)
+			if err != nil {
+				logger.Errorf("error: %s", res.Error())
+				w.WriteHeader(http.StatusUnprocessableEntity)
+				return
+			}
+		}
+
+		cl, _ := w.Write([]byte(data))
 		w.Header().Set(constants.ContentLengthHeader, string(cl))
 		w.Header().Set(constants.CacheControlHeader, fmt.Sprintf("public,max-age=%d", config.CacheTime()))
 	}
--- handler/image_test.go
+++ handler/image_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"context"
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -14,27 +15,30 @@ import (
 
 type ImageHandlerTestSuite struct {
 	suite.Suite
-	storage *storage.MockBaseStorage
+	deps        *service.Dependencies
+	storage     *storage.MockBaseStorage
+	manipulator *mockManipulator
 }
 
 func TestImageHandlerSuite(t *testing.T) {
 	suite.Run(t, new(ImageHandlerTestSuite))
 }
 
-func (s *ImageHandlerTestSuite) SetupSuite() {
+func (s *ImageHandlerTestSuite) SetupTest() {
 	s.storage = &storage.MockBaseStorage{}
+	s.manipulator = &mockManipulator{}
+	s.deps = &service.Dependencies{Storage: s.storage, Manipulator: s.manipulator}
 }
 
 func (s *ImageHandlerTestSuite) TestImageHandler() {
 	r, _ := http.NewRequest(http.MethodGet, "/image-valid", nil)
 	rr := httptest.NewRecorder()
 
-	s.storage.On("Get", mock.Anything, "/image-valid").Return([]byte("someData"), http.StatusOK, nil)
-	deps := service.Dependencies{Storage: s.storage}
+	s.storage.On("Get", mock.Anything, "/image-valid").Return([]byte("validData"), http.StatusOK, nil)
 
-	ImageHandler(&deps).ServeHTTP(rr, r)
+	ImageHandler(s.deps).ServeHTTP(rr, r)
 
-	assert.Equal(s.T(), "someData", rr.Body.String())
+	assert.Equal(s.T(), "validData", rr.Body.String())
 	assert.Equal(s.T(), http.StatusOK, rr.Code)
 }
 
@@ -43,10 +47,53 @@ func (s *ImageHandlerTestSuite) TestImageHandlerWithStorageGetError() {
 	rr := httptest.NewRecorder()
 
 	s.storage.On("Get", mock.Anything, "/image-invalid").Return([]byte(nil), http.StatusUnprocessableEntity, errors.New("error"))
-	deps := service.Dependencies{Storage: s.storage}
 
-	ImageHandler(&deps).ServeHTTP(rr, r)
+	ImageHandler(s.deps).ServeHTTP(rr, r)
 
 	assert.Equal(s.T(), "", rr.Body.String())
 	assert.Equal(s.T(), http.StatusUnprocessableEntity, rr.Code)
+}
+
+func (s *ImageHandlerTestSuite) TestImageHandlerWithQueryParameters() {
+	r, _ := http.NewRequest(http.MethodGet, "/image-valid?w=100&h=100", nil)
+	rr := httptest.NewRecorder()
+
+	params := make(map[string]string)
+	params["w"] = "100"
+	params["h"] = "100"
+	s.storage.On("Get", mock.Anything, "/image-valid").Return([]byte("validData"), http.StatusOK, nil)
+	s.manipulator.On("Process", mock.Anything, []byte("validData"), params).Return([]byte("processedData"), nil)
+
+	ImageHandler(s.deps).ServeHTTP(rr, r)
+
+	assert.Equal(s.T(), "processedData", rr.Body.String())
+	assert.Equal(s.T(), http.StatusOK, rr.Code)
+}
+
+func (s *ImageHandlerTestSuite) TestImageHandlerWithQueryParametersAndProcessingError() {
+	r, _ := http.NewRequest(http.MethodGet, "/image-valid?w=100&h=100", nil)
+	rr := httptest.NewRecorder()
+
+	params := make(map[string]string)
+	params["w"] = "100"
+	params["h"] = "100"
+	s.storage.On("Get", mock.Anything, "/image-valid").Return([]byte("validData"), http.StatusOK, nil)
+	s.manipulator.On("Process", mock.Anything, []byte("validData"), params).Return([]byte(nil), errors.New("error"))
+
+	ImageHandler(s.deps).ServeHTTP(rr, r)
+
+	assert.Equal(s.T(), "", rr.Body.String())
+	assert.Equal(s.T(), http.StatusUnprocessableEntity, rr.Code)
+}
+
+type mockManipulator struct {
+	mock.Mock
+}
+
+func (m *mockManipulator) Process(ctx context.Context, data []byte, params map[string]string) ([]byte, error) {
+	args := m.Called(ctx, data, params)
+	if args.Get(1) == nil {
+		return args.Get(0).([]byte), nil
+	}
+	return args.Get(0).([]byte), args.Get(1).(error)
 }
