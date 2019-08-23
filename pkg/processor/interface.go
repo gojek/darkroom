1c8547aaaf065cc2deed2db9207371632418673f
Albertus Angga Raharja <albertusangga.98@gmail.com>
2019-08-23 17:12:30 +0700

    Use shorter variable names in Processor interface (img instead of image)

--- pkg/processor/interface.go
+++ pkg/processor/interface.go
@@ -5,29 +5,26 @@ import "image"
 // Processor interface for performing operations on image bytes
 type Processor interface {
 	// Crop takes an image.Image, width, height and a CropPoint and returns the cropped image
-	Crop(image image.Image, width, height int, point CropPoint) image.Image
+	Crop(img image.Image, width, height int, point CropPoint) image.Image
 	// Resize takes an image.Image, width and height and returns the re-sized image
-	Resize(image image.Image, width, height int) image.Image
+	Resize(img image.Image, width, height int) image.Image
 	// GrayScale takes an input byte array and returns the grayscaled byte array or error
-	GrayScale(image image.Image) image.Image
-	// Blur takes an input byte array and returns the blurred byte array by the specified
-	// radius(<=1000) or error radius must be larger than 0
-	Blur(image image.Image, radius float64) image.Image
+	GrayScale(img image.Image) image.Image
 	// Watermark takes an input byte array, overlay byte array and opacity value
 	// and returns the watermarked image bytes or error
 	Watermark(base []byte, overlay []byte, opacity uint8) ([]byte, error)
 	// Flip takes an input image and returns the image flipped. The direction of flip
 	// is determined by the specified mode - 'v' for a vertical flip, 'h' for a horizontal flip and
 	// 'vh'(or 'hv') for both.
-	Flip(image image.Image, mode string) image.Image
+	Flip(img image.Image, mode string) image.Image
 	// Rotate takes an input image and returns a image rotated by the specified degrees.
 	// The rotation is applied clockwise, and fractional angles are supported.
-	Rotate(image image.Image, angle float64) image.Image
+	Rotate(img image.Image, angle float64) image.Image
 	// Decode takes a byte array and returns the image, extension, and error
 	Decode(data []byte) (img image.Image, format string, err error)
 	// Encode takes an image and extension and return the encoded byte array or error
-	Encode(image image.Image, format string) ([]byte, error)
+	Encode(img image.Image, format string) ([]byte, error)
 	// FixOrientation takes an image and it's EXIF orientation (if exist)
 	// and returns the image with its EXIF orientation fixed
-	FixOrientation(image image.Image, orientation int) image.Image
+	FixOrientation(img image.Image, orientation int) image.Image
 }
