93b31f38f71480a396c317a78c4489cc03f0891a
ajatprabha <ajat.prabha.leo@gmail.com>
2020-07-08 19:37:39 +0530

    implement detailed version command

--- cmd/version.go
+++ cmd/version.go
@@ -1,19 +1,32 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/gojek/darkroom/internal/version"
 	"github.com/spf13/cobra"
 )
 
 func newVersionCmd() *cobra.Command {
-	return &cobra.Command{
+	args := struct {
+		detailed bool
+	}{}
+	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Print version",
 		Long:  `Print version.`,
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			buildInfo := version.Build
-			cmd.Println(buildInfo.Version)
+			if args.detailed {
+				cmd.Println(fmt.Sprintf("Version:    %s", buildInfo.Version))
+				cmd.Println(fmt.Sprintf("Git Tag:    %s", buildInfo.GitTag))
+				cmd.Println(fmt.Sprintf("Git Commit: %s", buildInfo.GitCommit))
+				cmd.Println(fmt.Sprintf("Build Date: %s", buildInfo.BuildDate))
+			} else {
+				cmd.Println(buildInfo.Version)
+			}
 			return nil
 		},
 	}
+	cmd.PersistentFlags().BoolVarP(&args.detailed, "detailed", "a", false, "Print detailed version")
+	return cmd
 }
