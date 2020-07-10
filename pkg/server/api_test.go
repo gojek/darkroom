1a134a61a398823996dac3a4fbbb201cf76bfff5
ajatprabha <ajat.prabha.leo@gmail.com>
2020-07-11 01:19:31 +0530

    add test for LifeCycleHook

--- pkg/server/api_test.go
+++ pkg/server/api_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestNewServer(t *testing.T) {
@@ -13,4 +14,33 @@ func TestNewServer(t *testing.T) {
 		LifeCycleHook: nil,
 	})
 	assert.NotNil(t, s)
+}
+
+func TestLifeCycleHook(t *testing.T) {
+	init, deferred := false, false
+	s := NewServer(Options{
+		Handler: mux.NewRouter(),
+		Port:    3000,
+		LifeCycleHook: &LifeCycleHook{
+			initFunc:  func() { init = true },
+			deferFunc: func() { deferred = true },
+		},
+	})
+	assert.NotNil(t, s)
+	stopCh := make(chan struct{})
+	errCh := make(chan error)
+	go func() {
+		defer close(errCh)
+		err := s.Start(stopCh)
+		errCh <- err
+	}()
+
+	// wait for above goroutine to run
+	time.Sleep(1 * time.Second)
+	assert.True(t, init)
+	assert.False(t, deferred)
+	close(stopCh)
+
+	<-errCh
+	assert.True(t, deferred)
 }
