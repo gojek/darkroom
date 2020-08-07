5dc1a4e987ba9712a3a58a31ad944997c8461ff8
ajatprabha <ajat.prabha.leo@gmail.com>
2020-08-07 23:56:47 +0530

    add Storage.Get func test

--- pkg/storage/gcs/storage_test.go
+++ pkg/storage/gcs/storage_test.go
@@ -1,8 +1,14 @@
 package gcs
 
 import (
+	"context"
 	"github.com/stretchr/testify/suite"
+	"net/http"
 	"testing"
+)
+
+const (
+	validPath    = "path/to/valid-file"
 )
 
 type StorageTestSuite struct {
@@ -20,4 +26,12 @@ func TestStorageSuite(t *testing.T) {
 
 func (s *StorageTestSuite) TestNewStorage() {
 	s.NotNil(s.storage)
+}
+
+func (s *StorageTestSuite) TestStorage_Get() {
+	res := s.storage.Get(context.Background(), validPath)
+
+	s.Nil(res.Error())
+	s.Equal([]byte("someData"), res.Data())
+	s.Equal(http.StatusOK, res.Status())
 }
